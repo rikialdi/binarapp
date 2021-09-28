51fd82bbb95997f6f50865b4d3a73e1b2176cd8f
riki aldi pari <rikialdipari@gmail.com>
2021-09-28 16:30:45 +0700

    add

--- src/main/java/com/binar/binar/BinarApplication.java
+++ src/main/java/com/binar/binar/BinarApplication.java
@@ -8,12 +8,16 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@EntityScan("com.binar.binar.entity")
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
 @SpringBootApplication
+@OpenAPIDefinition
+//@EnableSwagger2
 public class BinarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BinarApplication.class, args);
